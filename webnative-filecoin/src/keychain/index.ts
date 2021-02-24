3f6d6824505429acc630a12aa6108cbc9ef53678
Daniel Holmgren <dtholmgren@gmail.com>
2021-02-23 19:19:36 -0700

    updates

--- webnative-filecoin/src/keychain/index.ts
+++ webnative-filecoin/src/keychain/index.ts
@@ -1,5 +1,15 @@
-import { Either, left, right } from "fp-ts/lib/Either";
-import Address from '../filecoin/address';
+// import Address from '../filecoin/address';
+import { FileSystem } from 'webnative/fs/filesystem'
+
+// API (for phase 2 stuff)
+// -----------------------
+// - Get Wallet
+// - Send
+//   + From wallet to lotus node
+//   + Probably just amount and address of wallet managed by Lotus node?
+// - Balances
+//   + Wallet in app
+//   + Wallet managed by Lotus node
 
 export enum Network {
   Main = 'f',
@@ -7,7 +17,7 @@ export enum Network {
 };
 
 export type BalanceStatement = {
-  [address: Address]:bigint;
+  balances: { [address: string]: number }
   blockheight: number;
   network: Network;
 }
@@ -17,33 +27,53 @@ export class InvalidRequestError extends Error {};
 
 export class Receipt {};
 
+export type ConstructorParams = {
+  fs: FileSystem
+  network?: Network
+}
+
+export type Address = string
+
 export class Keychain {
 
-  async generateAddress(
-    network: Network
-  ): Promise<Either<NetworkProviderError, Address>> {
+  fs: FileSystem
+  network: Network
 
+  constructor({ fs, network }: ConstructorParams) {
+    this.fs = fs
+    this.network = network || Network.Main
+  }
+
+  async generateAddress(): Promise<Address> { 
+    return 't3q5cgdg2b6uzazz7sbkdjqoafxzvuagbawh76wamwazupvvwzol7glitxs4e2j2wd5ncsg2mltrdt2t6gdisa'
   }
 
   // does not need to query the filecoin network to get updated balances
-  async getAddresses(
-    network: Network,
-  ): Promise<Either<Error, Address[]>> {};
+  async getAddresses(): Promise<Address[]> { 
+    return [
+      't3q5cgdg2b6uzazz7sbkdjqoafxzvuagbawh76wamwazupvvwzol7glitxs4e2j2wd5ncsg2mltrdt2t6gdisa',
+      't3wgxspnbaktqwk4fnruxzsne4cdu6olopflsowk7fvux6c5vxcxnwhoaimxpgtwc4q6gf27fzsik6axvjaugq'
+    ]
+  };
 
   // does perform an active network request to get the latest balances
-  async getBalances(
-    network: Network,
-    filter?: string // type can be improved probably
-  ): Promise<Either<NetworkProviderError | InvalidRequestError, BalanceStatement>> {
-
+  async getBalances(filter?: string): Promise<BalanceStatement> {
+    return {
+      balances: {
+        t3q5cgdg2b6uzazz7sbkdjqoafxzvuagbawh76wamwazupvvwzol7glitxs4e2j2wd5ncsg2mltrdt2t6gdisa: 2000,
+        t3wgxspnbaktqwk4fnruxzsne4cdu6olopflsowk7fvux6c5vxcxnwhoaimxpgtwc4q6gf27fzsik6axvjaugq: 1000
+      },
+      blockheight: 311003,
+      network: Network.Test,
+    }
   }
 
   async transfer(
     from: Address, // must be an address active in this keychain
     to: Address,   // can be any address on the same network
     amount: bigint // as integer in attoFIL = 10^(-18) FIL
-  ): Promise<Either<NetworkProviderError | InvalidRequestError, Receipt>> {
-
+  ): Promise<Receipt> {
+    return {} 
   }
 }
 
